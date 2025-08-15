effe371184f2429071a0ca18d4803f6548411074
wild-arch <mikemakorere@gmail.com>
2025-08-15 05:40:58 +0300

    type conversion 2.0

--- first.py
+++ first.py
@@ -3,8 +3,17 @@
 #print('hi ' + name + '!')
 
  #learn about type conversion
-birth_year = input("When were you born? ")
-print(type(birth_year))
-age = 2025 - int(birth_year)
-print(type(age))
-print("Mike, you must be  ", age,  " years old!")
+#birth_year = input("When were you born? ")
+#print(type(birth_year))
+#age = 2025 - int(birth_year)
+#print(type(age))
+#print("Mike, you must be  ", age,  " years old!")
+
+
+# Technical question
+
+weight_pounds = input("What is your weight in pounds? ")
+
+weight_kg = float(weight_pounds) * 0.45359237
+
+print("You weigh ", weight_kg, " kilograms.")
