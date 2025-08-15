7531b2838a9c015cba2db6971736fc5e0e5b218a
wild-arch <mikemakorere@gmail.com>
2025-08-15 05:14:26 +0300

    learning type conversion

--- first.py
+++ first.py
@@ -1,3 +1,10 @@
 name = input("What is your name? ")
 
 print('hi ' + name + '!')
+
+ #learn about type conversion
+
+age = input("How old are you? ")
+# Convert age to an integer
+age = int(age)
+print('You are ' + str(age) + ' years old!')
