e817088e00cf915b0452191b70e6f2d6c519f9b0
wild-arch <mikemakorere@gmail.com>
2025-08-18 02:47:06 +0300

    looping in list

--- first.py
+++ first.py
@@ -1,4 +1,8 @@
 # lists
 
-names = ['Alice', 'Bob', 'Charlie', 'David']
-print(names[-2:])
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 79, 2029, 1000]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print("The maximum number is:", max)
