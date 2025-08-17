c47cd4904a4672d324f3097581fefe649faac342
wild-arch <mikemakorere@gmail.com>
2025-08-18 01:50:49 +0300

    lists

--- first.py
+++ first.py
@@ -1,44 +1,4 @@
-#name = input("What is your name? ")
-from operator import length_hint
+# lists
 
-#print('hi ' + name + '!')
-
- #learn about type conversion
-#birth_year = input("When were you born? ")
-#print(type(birth_year))
-#age = 2025 - int(birth_year)
-#print(type(age))
-#print("Mike, you must be  ", age,  " years old!")
-
-
-# Technical question
-
-#weight_pounds = input("What is your weight in pounds? ")
-
-#weight_kg = float(weight_pounds) * 0.45359237
-
-#print("You weigh ", weight_kg, " kilograms.")
-
-#msg = '''
-#dear mike,
-
-#you are the f* best!
-#sincerely,
-#your biggest fan
-#mike
-
-
-
-    # weight converter
-
-
-
-
-
-# for loop nested
-star = [2, 2, 2, 2, 5]
-for x in star:
-    output = ''
-    for y in range(x):
-        output += '*'
-    print(output)
+names = ['Alice', 'Bob', 'Charlie', 'David']
+print(names[-2:])
