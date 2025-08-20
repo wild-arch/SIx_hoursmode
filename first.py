89d21a25c70bf1cef11ccb2751a151074145314f
wild-arch <mikemakorere@gmail.com>
2025-08-20 04:31:04 +0300

    learning functions

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
-# lists
+# functions
+def basic_tryout():
+    print("Hello World")
+    print("Hello again")
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 79, 2029, 1000]
-max = numbers[0]
-for number in numbers:
-    if number > max:
-        max = number
-print("The maximum number is:", max)
+print("mike")
+basic_tryout()
+print("hi")
