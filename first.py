aaa9295d3231a493598df1663260cd9dffa6fd54
wild-arch <mikemakorere@gmail.com>
2025-08-21 12:00:24 +0300

    trying out conditional statements

--- first.py
+++ first.py
@@ -1,9 +1,2 @@
-name = input("What's your name? ").lower().title()
-
-match name:
-    case "Harry" | "Ron" | "Hermione":
-        print("Gryffindor")
-    case "Draco":
-        print("Slytherin")
-    case _:
-        print("Who?")
+for i in [0, 1, 2]:
+    print("meow")
